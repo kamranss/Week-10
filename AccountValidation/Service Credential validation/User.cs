c38e70fe2af10609b4a10db812bd44b51563067b
kamranss <kamranss@code.edu.az>
2022-11-29 00:41:07 +0400

    Feature: Week 10 User Password Validation checker

---
+++ AccountValidation/Service Credential validation/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AccountValidation.Service_Credential_validation
+{
+    internal class User:IAccount
+    {
+        static int idCount = 0;
+        public int Id { get; private set; }
+        public int Fullname { get; set; }
+        public int Email { get; set; }
+        public int Password { get; set; }
+
+        Regex Passwordcheck = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*d)[a-zA-Zd]{8,}$");
+
+        public User()
+        {
+            idCount++;
+            Id = idCount;
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            if (Passwordcheck.IsMatch(password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string ShowInfo()
+        {
+            return
+            (Console.WriteLine($"User Id ->> {Id}, User Fullname ->> {Fullname}, User Email ->> {Email}");
+        }
+    }
+}
